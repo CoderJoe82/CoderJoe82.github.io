7f25ee0ab133f2eaece4c303f9fe1fe2997ee4f6
CoderJoe82 <coderjoe82@gmail.com>
2021-07-11 19:34:35 -0400

    pleeeeeease let me do this.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import React, { Component } from 'react'
+import Routes from './components/Routes/routes'
+
+class App extends Component {
+  render() {
+    return (
+      <div id = "App">
+        <Routes />
+      </div>
+    )
+  }
+}
+
+export default App
