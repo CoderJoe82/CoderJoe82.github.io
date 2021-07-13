44cf46ed16037b74132d82b4f7f1a51eb75bcd97
CoderJoe82 <coderjoe82@gmail.com>
2021-07-13 18:43:02 -0400

    ppllllease

--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -7,7 +7,7 @@ class Routes extends Component {
         return (
             <Router>
                 <Switch>
-                    <Route path = "/" exact component = {LandingPage}/>
+                    <Route path = "/Portfolio" exact component = {LandingPage}/>
                 </Switch>
             </Router>
         )
